67be115b69c8a782d1f92617e1384e6dc4b94e1c
Michali Izhaky <michaliizhaky@gmail.com>
2026-03-24 08:22:47 -0700

    Add biomarkers to config

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv("data/raw/PUB2023_scraped.csv.gz")
+
+GENE_COLS = [c for c in df.columns if 'mtb' in col]
 SEX_COL = ['sex']
 BMI_AGE_COLS = ['BMI','age']
 TARGET_COLS = 'has_diabetes'
