658f049b3fddaef8a1732a2ed4bde71a92c62138
Michali Izhaky <michaliizhaky@gmail.com>
2026-03-24 08:29:29 -0700

    Fix bug

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv("../data/raw/PUB2023_scraped.csv.gz")
 
-GENE_COLS = [c for c in df.columns if 'mtb' in col]
+GENE_COLS = [c for c in df.columns if 'mtb' in c]
 SEX_COL = ['sex']
 BMI_AGE_COLS = ['BMI','age']
 TARGET_COLS = 'has_diabetes'
