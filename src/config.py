02e0d93d5eb8d9b5e803cc99d27c257a9af19ba9
Michali Izhaky <michaliizhaky@gmail.com>
2026-03-24 08:26:44 -0700

    Move

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv("data/raw/PUB2023_scraped.csv.gz")
+
+GENE_COLS = [c for c in df.columns if 'mtb' in col]
+SEX_COL = ['sex']
+BMI_AGE_COLS = ['BMI','age']
+TARGET_COLS = 'has_diabetes'
+
+VAR_THRESHOLD = 0.1
+CORR_THRESHOLD = 0.9
+STABILITY_PCT = 80
