36bf61ab90f4fa0249aeb96bc7e3b8280a02c6a5
Michali Izhaky <michaliizhaky@gmail.com>
2026-03-24 08:39:25 -0700

    Add column designations

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ df = pd.read_csv("../data/raw/PUB2023_scraped.csv.gz")
 GENE_COLS = [c for c in df.columns if 'mtb' in c]
 SEX_COL = ['sex']
 BMI_AGE_COLS = ['BMI','age']
-TARGET_COLS = 'has_diabetes'
+TARGET_COL = 'has_diabetes'
 
 VAR_THRESHOLD = 0.1
 CORR_THRESHOLD = 0.9
